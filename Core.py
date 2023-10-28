16ecda5b595f7efa3e302f33162a411d13c2e81e
KioZZero <niels.diab@gmail.com>
2023-10-28 22:35:26 +0200

    # partout

--- Core.py
+++ Core.py
@@ -1,33 +1,41 @@
 import pygame 
 from sys import exit
 
+#initiation du module Pygame
 pygame.init()
+#definition de la taille du jeux
 screen = pygame.display.set_mode((800,600))
+#titre afficher en haut de la page
 pygame.display.set_caption('Mein Gott')
+#utile pour éviter que la fenetre ne se ferme ///PAS TOUCHEE
 clock = pygame.time.Clock()
 
+#textures des fonds différents jour/nuit que l'on appelle puis recadre/transforme
 skyday_surface = pygame.image.load('Paysage-jour.png').convert()
 skyday_surface = pygame.transform.scale(skyday_surface,(800,600))
 skynight_surface = pygame.image.load('Paysage-nuit.png').convert()
 skynight_surface = pygame.transform.scale(skynight_surface,(800,600))
 
+#texture du 'sol' pour le Player
 ground_surface = pygame.image.load('Ground.png').convert_alpha()
 ground_surface = pygame.transform.scale(ground_surface,(800,600))
 
+#texture du Player
 player_surface = pygame.image.load('Player.png').convert_alpha()
 player_surface = pygame.transform.scale(player_surface,(160,120))
-player_rect = player_surface.get_rect(midbottom = (80,300))
 
+#position x et y du Player
 pl_x_pos = 400
 pl_y_pos = 400
 
+#Initiation du jeu
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         
-    
+    #controles des mouvements x y du Player
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
         pl_x_pos+=2
