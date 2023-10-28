f763e95f9ae9b56a93a0240da3044dfed362eae0
KioZZero <niels.diab@gmail.com>
2023-10-28 22:02:47 +0200

    tr

--- Core.py
+++ Core.py
@@ -3,7 +3,7 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Stupid Choices aaaaaaaaaaa')
+pygame.display.set_caption('Mein Gott')
 clock = pygame.time.Clock()
 
 sky_surface = pygame.image.load('Sky.png').convert()
@@ -28,22 +28,22 @@ while True:
     
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
-        pl_x_pos+=3
+        pl_x_pos+=2
         
     if pressed[pygame.K_a] or pressed[pygame.K_LEFT]:
-        pl_x_pos-=3
+        pl_x_pos-=2
 
     if pressed[pygame.K_w] or pressed[pygame.K_UP]:
         if pl_y_pos<=410:
             pl_y_pos=410
         else:
-            pl_y_pos-=2
+            pl_y_pos-=1
 
     if pressed[pygame.K_s] or pressed[pygame.K_DOWN]:
         if pl_y_pos>=500:
             pl_y_pos=500
         else:
-            pl_y_pos+=2
+            pl_y_pos+=1
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,100))
