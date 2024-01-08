3e0c442c411e0e25e133dab0bd4199f230d3044f
Cyprien <niels.diab@gmail.com>
2024-01-08 20:29:55 +0100

    tr

--- Core.py
+++ Core.py
@@ -9,7 +9,7 @@ pygame.init()
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
-BG = pygame.image.load("assets/Background.png")
+BG = pygame.image.load("Background.png")
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
