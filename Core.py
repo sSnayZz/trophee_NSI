85999a68cf022c49dcfe7510d1cfd65b4784e3c0
KioZZero <niels.diab@gmail.com>
2024-01-14 15:41:53 +0100

    quitter marche

--- Core.py
+++ Core.py
@@ -6,9 +6,9 @@ pygame.init()
 #definition de la taille du jeux
 Size = (1920/2,1080/2)
 screen = pygame.display.set_mode(Size)
-button_width,button_height=100,50
+button_width,button_height=200,50
 center_x,center_y = (Size[0] - button_width) // 2,(Size[1] - button_height) // 2
-button_y_play,button_y_option,button_y_leave=center_y//2,center_y,center_y*2
+button_y_play,button_y_option,button_y_leave=center_y//2,center_y,center_y+center_y//2
 
 #titre afficher en haut de la page
 pygame.display.set_caption('Mein Gott')
@@ -35,9 +35,8 @@ def draw_button(x, y, width, height, color, text, text_color):
     text_rect = button_text.get_rect(center=(x + width / 2, y + height / 2))
     screen.blit(button_text, text_rect)
     
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)    
+button_color=(255,255,255)   
+button_text_color=(0,0,0)
 
 #Initiation du jeu
 def main_menu():
@@ -48,23 +47,18 @@ def main_menu():
                 exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                  #Vérifier si le clic de la souris est sur le bouton "Jouer"
-                if center_x <= event.pos[0] <= center_x+button_width and 100 <= event.pos[1] <= 150:
+                if center_x <= event.pos[0] <= center_x+button_width and button_y_play <= event.pos[1] <= button_y_play+button_height:
                     print("Le bouton 'Jouer' a été cliqué! passer a scene suivante")
-            
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                 #Vérifier si le clic de la souris est sur le bouton "Jouer"
-                if center_x <= event.pos[0] <= center_x+button_width and 100 <= event.pos[1] <= 150:
-                    print("Le bouton 'Jouer' a été cliqué! passer a scene suivante")
-            
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                 #Vérifier si le clic de la souris est sur le bouton "Jouer"
-                if center_x <= event.pos[0] <= center_x+button_width and 100 <= event.pos[1] <= 150:
-                    print("Le bouton 'Jouer' a été cliqué! passer a scene suivante")
-        
+                if center_x <= event.pos[0] <= center_x+button_width and button_y_option <= event.pos[1] <= button_y_option+button_height:
+                    print("Le bouton 'truc' a été cliqué! passer a scene suivante")
+                if center_x <= event.pos[0] <= center_x+button_width and button_y_leave <= event.pos[1] <= button_y_leave+button_height:
+                    pygame.quit()
+                    exit()
+                    
         screen.blit(BG,(0,0))
-        draw_button(center_x, button_y_play, button_width, button_height, GREEN, "Jouer", BLACK)
-        draw_button(center_x, button_y_option, button_width, button_height, GREEN, "option", BLACK)
-        draw_button(center_x, button_y_leave, button_width, button_height, GREEN, "quitter", BLACK)
+        draw_button(center_x, button_y_play, button_width, button_height, button_color, "Jouer", button_text_color)
+        draw_button(center_x, button_y_option, button_width, button_height, button_color, "option", button_text_color)
+        draw_button(center_x, button_y_leave, button_width, button_height, button_color, "quitter", button_text_color)
         pygame.display.update()
         clock.tick(120)
 
