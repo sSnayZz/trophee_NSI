d9b3253f35f9b48ba694d122af44534ad14a7995
Cyprien <niels.diab@gmail.com>
2024-01-18 19:30:37 +0100

    truc

--- Core.py
+++ Core.py
@@ -10,25 +10,18 @@ button_width,button_height=200,50
 center_x,center_y = (Size[0] - button_width) // 2,(Size[1] - button_height) // 2
 button_y_play,button_y_option,button_y_leave=center_y//2,center_y,center_y+center_y//2
 
-running=True
 #titre afficher en haut de la page
-pygame.display.set_caption('The temple of the Idole')
+pygame.display.set_caption('The Temple Of The Idol')
 
 #utile pour éviter que la fenetre ne se ferme ///PAS TOUCHEE
 clock = pygame.time.Clock()
 
 
+#expo x^3
 
 BG = pygame.image.load('Background.png').convert()
 BG = pygame.transform.scale(BG,Size)
 
-#for x in range(11):
-#	print(255 * (x/10)**2)
-#fonction utile 
-
-
-
-    
 #texture du Player
 # player_surface = pygame.image.load('Skin-R.png').convert_alpha()
 # player_surface = pygame.transform.scale(player_surface,(display_x,display_y))
@@ -43,17 +36,21 @@ def draw_button(x, y, width, height, color, pressed_color, text, text_color,is_p
     text_rect = button_text.get_rect(center=(x + width / 2, y + height / 2))
     screen.blit(button_text, text_rect)
 
-
 button_color=(255,255,255)  
 button_color_pressed=(200,200,200) 
 button_text_color=(0,0,0)
-
+running=True
 
 
 #Initiation du jeu
 def main_menu():
     button_pressed = False
-    while running==True:
+    while True:
+        pygame.draw.rect(screen, (0,0,0,255), ((0,0)), Size)
+
+        if Run==0:
+            break
+    while running is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -72,8 +69,7 @@ def main_menu():
                     exit()
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 button_pressed = False
-                    
-        black_starting_screen(starting_color+=1)
+
         screen.blit(BG,(0,0))
         #play
         draw_button(center_x, button_y_play, button_width, button_height, button_color,button_color_pressed, "Jouer", button_text_color, button_pressed)
